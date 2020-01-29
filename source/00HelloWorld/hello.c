42681eccc2d22f0a6d0fcce8d12f4c538d87160d
sotrteacher <lamberto9@gmail.com>
2020-01-29 14:15:58 -0600

    Make file recently added

---
+++ source/00HelloWorld/hello.c
@@ -0,0 +1,31 @@
+/**hello.c
+   Para editar este archivo, use:
+$ vim hello.c
+*/
+int printf(char *,...);
+int atoi(const char *);
+
+
+int main(int argc,char *argv[])
+{
+  int intA;
+  if(argc<2){
+    printf("FORMA DE USO:%s <n><--|\n",argv[0]);
+    return 1;
+  }
+  intA=atoi(argv[1]);
+  printf("%i\n",intA);
+  for(intA=0;intA<argc;++intA)
+  {
+    printf("%5i%20s\n",intA,argv[intA]);
+  }
+/*  return printf("         HOLA MUNDO C from Debian running on WSL!\n");
+*/
+  return 0;
+}/*end main()*/
+
+/** Alternativamente, podemos utilizar el programa Cygwin */
+/** Para instalarlo: 
+    User:cursos
+    pw:Aprender1
+*/
