f61a148bd4e9dc6ba659b9e716f8f9d02649a782
sotrteacher <lamberto9@gmail.com>
2020-02-04 15:03:01 -0600

    Fabrice Bellard tcc sources added

--- source/00HelloWorld/hello.c
+++ source/00HelloWorld/hello.c
@@ -2,15 +2,19 @@
    Para editar este archivo, use:
 $ vim hello.c
 */
-#include "./include/funciong.h"
-int printf(char *,...);
+//#include "./include/funciong.h"
+#include <funciong.h> /*Add -I./include in your make file*/
+#include <stdio.h>
+//int printf(char *,...);
 int atoi(const char *);
 
 
 int main(int argc,char *argv[])
 {
   int intA;
+#ifdef TEST1
   printf("%c\n",g(29%7));
+#endif
   if(argc<2){
     printf("FORMA DE USO:%s <n><--|\n",argv[0]);
     return 1;
