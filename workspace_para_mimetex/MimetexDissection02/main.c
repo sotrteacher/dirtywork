e0936d70ec3e90cc21c1f2e34a6d01104f0cc03a
lambertomaza <lmaza@ipn.mx>
2022-11-08 11:07:02 -0600

    2022.11.08

--- workspace_para_mimetex/MimetexDissection02/main.c
+++ workspace_para_mimetex/MimetexDissection02/main.c
@@ -1,8 +1,179 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define ABCD
+#ifdef ABCD
+//mimetex.c 429
+#include <string.h>
+//mimetex.c 573
+#include "mimetex.h"
+/* --- mimetex.c 598
+ * internal buffer sizes
+ * --------------------- */
+#if !defined(MAXEXPRSZ)
+  #define MAXEXPRSZ (32768-1)		/*max #bytes in input tex expression*/
+#endif
+int	iscachecontenttype = 0;		/* true to cache mime content-type */
+char	contenttype[2048] = "\000";	/* content-type:, etc buffer */
+//mimetex.c  616
+#if !defined(MAXGIFSZ)
+#define MAXGIFSZ 131072               /* max #bytes in output GIF image */
+#endif
+//mimetex.c 819
+/* --- check whether or not \input, \counter, \environment permitted --- */
+  #if defined(DEFAULTSECURITY)            /* default security specified */
+    #define EXPLICITDEFSECURITY           /* don't override explicit default */
+  #else                                   /* defualt security not specified */
+    #define DEFAULTSECURITY (8)           /* so set default security level */
+  #endif
+  #if defined(INPUTREFERER)               /*http_referer's permitted to \input*/
+    #if !defined(INPUTSECURITY)           /* so we need to permit \input{} */
+      #define INPUTSECURITY (99999)       /* make sure SECURITY<INPUTSECURITY */
+    #endif
+  #else                                   /* no INPUTREFERER list supplied */
+    #define INPUTREFERER NULL             /* so init it as NULL pointer */
+  #endif
+  #if !defined(INPUTPATH)                 /* \input{} paths permitted for... */
+    #define INPUTPATH NULL                /* ...any referer */
+  #endif
+  #if !defined(INPUTSECURITY)             /* \input{} security not specified */
+    #if defined(INPUTOK)                  /* but INPUTOK flag specified */
+      #define INPUTSECURITY (99999)       /* so enable \input{} */
+    #if !defined(EXPLICITDEFSECURITY)   /* don't override explicit default */
+       #undef  DEFAULTSECURITY           /* but we'll override our default */
+       #define DEFAULTSECURITY (99999)   /*let -DINPUTOK enable \counter,etc*/
+    #endif
+  #else                                 /* else no \input{} specified */
+    #define INPUTSECURITY DEFAULTSECURITY /* set default \input security */
+  #endif
+  #endif
 
-int main()
+//mimetex.c 875
+/* --- treat +'s in query string as blanks? --- */
+#if defined(PLUSBLANK)                  /* + always interpreted as blank */
+   #define ISPLUSBLANK 1
+#else
+   #if defined(PLUSNOTBLANK)             /* + never interpreted as blank */
+     #define ISPLUSBLANK 0
+   #else                                 /* program tries to determine */
+   #define ISPLUSBLANK (-1)
+   #endif
+ #endif
+
+//mimetex.c 902
+GLOBAL(int,inputseclevel,INPUTSECURITY); /* \input{} security level */
+
+//mimetex.c 924
+/* -------------------------------------------------------------------------
+control flags and values
+-------------------------------------------------------------------------- */
+/* --- declare global symbol --- */
+  // #ifdef INITVALS
+  //   #define GLOBAL(type,variable,value) STATIC type variable = value
+  //   /* #define GLOBAL(type,variable,value) STATIC type variable = (value) */
+  //   /* #define SHARED(type,variable,value) type variable = (value) */
+  // #else
+  //   #define GLOBAL(type,variable,value) STATIC type variable
+  //   /* #define SHARED(type,variable,value) STATIC type variable */
+  // #endif
+GLOBAL(int,isquery,0);			/* true=cgi?query; false=commandline*/
+// mimetex.c 970
+GLOBAL(int,isplusblank,ISPLUSBLANK);    /*interpret +'s in query as blanks?*/
+// mimetex 981
+GLOBAL(int,ispbmpgm,0);                 /* true for pbm/pgm instead of gif */
+#endif // ABCD
+// mimetex.c 16679
+int main(int argc, char *argv[])
 {
-    printf("Hello world!\n");
-    return 0;
+#ifdef ABCD
+//mimetex.c 16627
+ int	iarg=0, argnum=0,		/*argv[] index for command-line args*/
+	exprarg = 0,			/* argv[] index for expression */
+	infilearg = 0,			/* argv[] index for infile */
+	nswitches = 0,			/* number of -switches */
+	isstopsignal = 0,		/* true after stopsignal found */
+	isstrict = 1/*iswindows*/,	/* true for strict arg checking */
+					/*nb, windows has apache "x -3" bug*/
+	nargs=0, nbadargs=0,		/* number of arguments, bad ones */
+	maxbadargs = (isstrict?0:1),	/*assume query if too many bad args*/
+	isgoodargs = 0;			/* true to accept command-line args*/
+//mimetex.c 16675
+/* -------------------------------------------------------------------------
+Allocations and Declarations
+-------------------------------------------------------------------------- */
+/* --- expression to be emitted --- */
+ static	char exprbuffer[MAXEXPRSZ+1] = "f(x)=x^2"; /* input TeX expression */
+ char	*expression = exprbuffer;	/* ptr to expression */
+int	/*isquery = 0, (now global)*/	/* true if input from QUERY_STRING */
+	isqempty = 0,			/* true if query string empty */
+	isqforce = 0,			/* true to force query emulation */
+	isqlogging = 0,			/* true if logging in query mode */
+	isformdata = 0,			/* true if input from html form */
+	isinmemory = 1,			/* true to generate image in memory*/
+	isdumpimage = 0,		/* true to dump image on stdout */
+	isdumpbuffer = 0;		/* true to dump to memory buffer */
+//mimetex.c 16730
+char    *gif_outfile = (char *)NULL,    /* gif output defaults to stdout */
+         gif_buffer[MAXGIFSZ] = "\000",  /* or gif written in memory buffer */
+         cachefile[256] = "\000",        /* full path and name to cache file*/
+         *md5hash=NULL, *md5str();       /* md5 hash of expression */
+//mimetex 16739
+char    *pbm_outfile = (char *)NULL;    /* output file defaults to stdout */
+
+//mimetex.c 16780
+ inputseclevel = INPUTSECURITY;          /* security level for \input{} */
+
+//mimetex.c 16819
+/* ---
+ * process command-line input args (if not a query)
+ * ------------------------------------------------ */
+ if ( !isquery                           /* don't have an html query string */
+ ||   ( /*isqempty &&*/ argc>1) )        /* or have command-line args */
+  {
+
+
+//mimetex.c 16890
+/* ---
+  * decide whether command-line input overrides query_string
+  * -------------------------------------------------------- */
+ if ( isdumpimage > 2 ) nbadargs++;	/* duplicate/conflicting -switch */
+ isgoodargs =  ( !isstrict		/*good if not doing strict checking*/
+  || !isquery				/* or if no query, must use args */
+  || (nbadargs<nargs && nbadargs<=maxbadargs) ); /* bad args imply query */
+ /* ---
+  * take expression from command-line args
+  * -------------------------------------- */
+ if ( isgoodargs && exprarg > 0		/* good expression on command line */
+ &&   infilearg <= 0 )			/* and not given in input file */
+  if ( !isquery				/* no conflict if no query_string */
+  ||   nswitches > 0 )			/* explicit -switch(es) also given */
+   { strncpy(expression,argv[exprarg],MAXEXPRSZ); /*expr from command-line*/
+     expression[MAXEXPRSZ] = '\000';	/* make sure it's null terminated */
+     isquery = 0; }			/* and not from a query_string */
+// mimetex.c 16924
+ /* ---
+  * xlate +++'s to blanks only if query
+  * ----------------------------------- */
+ if ( !isquery ) isplusblank = 0;	/* don't xlate +++'s to blanks */
+ /* ---
+  * check if emulating query (for testing)
+  * -------------------------------------- */
+ if ( isqforce ) isquery = 1;		/* emulate query string processing */
+ /* ---
+  * check if emitting pbm/pgm graphic
+  * --------------------------------- */
+ if ( isgoodargs && ispbmpgm > 0 )	/* have a good -g arg */
+  if ( 1 && gif_outfile != NULL )	/* had an -e switch with file */
+   if ( *gif_outfile != '\000' )	/* make sure string isn't empty */
+     { pbm_outfile = gif_outfile;	/* use -e switch file for pbm/pgm */
+       gif_outfile = (char *)NULL;	/* reset gif output file */
+       /*isdumpimage--;*/ }		/* and decrement -e count */
+//mimetex.c 16941
+ /* ---
+  * permit \input, etc in command-line mode
+  * ------------------------------------------ */
+ inputseclevel = (99999);		/* \input allowed */
+ } /* --- end-of-if(!isquery) --- */
+#endif // ABCD
+ printf("Hello world!\n");
+ return 0;
 }
